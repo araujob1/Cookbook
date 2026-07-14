9caf13fc026af728143f092e82a2061fbd9cfff9
Yago Araujo <y.araujob1@gmail.com>
2026-07-14 14:01:51 -0300

    feat(infrastructure): add initial users table migration

---
+++ src/Backend/Cookbook.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Cookbook.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.CREATE_TABLE_USER, "Create users table")]
+public class Version0000001 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("users")
+            .WithColumn("name").AsString(100).NotNullable()
+            .WithColumn("email").AsString(255).NotNullable()
+            .WithColumn("password_hash").AsString(2000).NotNullable();
+
+        Execute.Sql("""
+            CREATE UNIQUE INDEX ix_users_email
+            ON users (email)
+            WHERE is_active = TRUE
+            """);
+    }
+}
---
+++ src/Backend/Cookbook.Infrastructure/Migrations/Versions/VersionBase.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+using FluentMigrator.Builders.Create.Table;
+
+namespace Cookbook.Infrastructure.Migrations.Versions;
+
+public abstract class VersionBase : ForwardOnlyMigration
+{
+    protected ICreateTableColumnOptionOrWithColumnSyntax CreateTable(string tableName)
+    {
+        return Create.Table(tableName)
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey()
+            .WithColumn("is_active").AsBoolean().NotNullable();
+    }
+}
