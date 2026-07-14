feebf05b8e2c309aa508a431d950124e078d9632
Yago Araujo <y.araujob1@gmail.com>
2026-07-14 14:02:12 -0300

    feat(infrastructure): constrain user persistence fields

--- src/Backend/Cookbook.Infrastructure/DataAccess/Configurations/UserConfiguration.cs
+++ src/Backend/Cookbook.Infrastructure/DataAccess/Configurations/UserConfiguration.cs
@@ -12,12 +12,17 @@ internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(user => user.Name)
             .HasConversion(
                 name => name.Value,
-                value => new UserName(value));
+                value => new UserName(value))
+            .HasMaxLength(100);
 
         builder.Property(user => user.Email)
             .HasConversion(
                 email => email.Value,
-                value => new Email(value));
+                value => new Email(value))
+            .HasMaxLength(255);
+
+        builder.Property(user => user.PasswordHash)
+            .HasMaxLength(2000);
 
         builder.HasIndex(user => user.Email)
             .IsUnique()
