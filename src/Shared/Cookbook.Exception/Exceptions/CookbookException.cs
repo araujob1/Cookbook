4144dc919549cfa8c7c2e46a0f754eba9f2e8270
Yago Araujo <y.araujob1@gmail.com>
2026-07-15 14:59:11 -0300

    feat(exception): add localized validation errors

---
+++ src/Shared/Cookbook.Exception/Exceptions/CookbookException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace Cookbook.Exception.Exceptions;
+
+public abstract class CookbookException : System.Exception
+{
+    public abstract IList<string> ErrorMessages { get; }
+    public abstract HttpStatusCode StatusCode { get; }
+}
---
+++ src/Shared/Cookbook.Exception/Exceptions/ErrorOnValidationException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Cookbook.Exception.Exceptions;
+
+public sealed class ErrorOnValidationException(IList<string> errorMessages) : CookbookException
+{
+    public override IList<string> ErrorMessages => errorMessages;
+
+    public override HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+}
