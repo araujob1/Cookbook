1b1f0332444248bb756966ab6bda2be6bdd21329
Yago Araujo <y.araujob1@gmail.com>
2026-07-22 17:36:50 -0300

    feat(application): register user registration services

---
+++ src/Backend/Cookbook.Application/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using Cookbook.Application.UseCases.User.Register;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cookbook.Application.Extensions;
+
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+
+        return services;
+    }
+}
