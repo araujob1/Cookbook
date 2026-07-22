236a555dd91c91dfe7d4323e5f94fe6c8dc8f776
Yago Araujo <y.araujob1@gmail.com>
2026-07-22 17:36:49 -0300

    feat(domain): add string emptiness extensions

---
+++ src/Backend/Cookbook.Domain/Extensions/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cookbook.Domain.Extensions;
+
+public static class StringExtension
+{
+    public static bool IsEmpty([NotNullWhen(false)] this string? value) =>
+        string.IsNullOrWhiteSpace(value);
+
+    public static bool IsNotEmpty([NotNullWhen(true)] this string? value) =>
+        !string.IsNullOrWhiteSpace(value);
+}
