0d6759068a279364e310251db822e5d41b82c286
Yago Araujo <y.araujob1@gmail.com>
2026-08-18 20:49:06 -0300

    feat(api): expose login endpoint

---
+++ src/Backend/Cookbook.Api/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Cookbook.Application.UseCases.Login;
+using Cookbook.Communication.Requests;
+using Cookbook.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookbook.Api.Controllers;
+
+public sealed class AuthController : CookbookBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login(
+        [FromBody] RequestLoginJson request,
+        [FromServices] ILoginUseCase useCase)
+    {
+        var response = await useCase.ExecuteAsync(request);
+
+        return Ok(response);
+    }
+}
