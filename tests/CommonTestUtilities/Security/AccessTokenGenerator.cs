b2a3ce9031473af407d1ea7f80513a3ccb750a13
Yago Araujo <y.araujob1@gmail.com>
2026-08-20 20:10:48 -0300

    test(common): add access token generator mock

---
+++ tests/CommonTestUtilities/Security/AccessTokenGenerator.cs
@@ -0,0 +1,20 @@
+﻿using CommonTestUtilities.Generators;
+using Cookbook.Domain.Entities;
+using Cookbook.Domain.Security.Tokens;
+using Moq;
+
+namespace CommonTestUtilities.Security;
+
+public sealed class AccessTokenGenerator
+{
+    public static IAccessTokenGenerator Build(string? accessToken = null)
+    {
+        accessToken ??= TextGenerator.Words(20);
+
+        var mock = new Mock<IAccessTokenGenerator>();
+
+        mock.Setup(x => x.Generate(It.IsAny<User>())).Returns(accessToken);
+
+        return mock.Object;
+    }
+}
