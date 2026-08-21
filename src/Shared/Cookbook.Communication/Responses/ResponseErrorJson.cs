4a8f5b3bb7ee75f47487aca167d040ea920ee726
Yago Araujo <y.araujob1@gmail.com>
2026-08-21 11:31:29 -0300

    feat(communication): expose access token expiration errors

--- src/Shared/Cookbook.Communication/Responses/ResponseErrorJson.cs
+++ src/Shared/Cookbook.Communication/Responses/ResponseErrorJson.cs
@@ -1,6 +1,11 @@
 ﻿namespace Cookbook.Communication.Responses;
 
-public sealed record ResponseErrorJson(IList<string> ErrorMessages)
+public sealed record ResponseErrorJson(
+    IList<string> ErrorMessages,
+    bool AccessTokenExpired = false)
 {
     public ResponseErrorJson(string errorMessage) : this([errorMessage]) { }
+
+    public ResponseErrorJson(string errorMessage, bool accessTokenExpired)
+        : this([errorMessage], accessTokenExpired) { }
 };
